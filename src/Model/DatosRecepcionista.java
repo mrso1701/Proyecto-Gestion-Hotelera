f8608c7df8b6ed988f67dcf24fd01c14bf74fbfe
Isabella Torres Yepes <isatorresyepes24@gmail.com>
2026-04-25 16:31:25 -0500

    feat: agrege una nueva clase llamada Datos para los arrays de recepcionista

---
+++ src/Model/DatosRecepcionista.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.List;
+import java.util.ArrayList;
+public class DatosRecepcionista {
+    
+    public static List<Cliente> listaClientes = new ArrayList<>();
+    public static List<Habitacion> listaHabitaciones = new ArrayList<>();
+    public static List<Reserva> listaReservas = new ArrayList<>();
+    public static List<Consumo> listaConsumos = new ArrayList<>();
+    public static List<TipoHabitacion> listaTipoHabitaciones = new ArrayList<>();
+    
+}
