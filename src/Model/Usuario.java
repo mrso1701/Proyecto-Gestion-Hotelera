9fa666372e7447764f5c73788267fe86ac510b89
mrso1701 <andresrodriguez172008@gmail.com>
2026-04-30 19:26:39 -0500

    feature: Creacion de la clase modelo Usuario

--- src/Model/Usuario.java
+++ src/Model/Usuario.java
@@ -1,5 +1,81 @@
 package Model;
 
 public class Usuario {
+    private String email; 
+    private String nombre;
+    private String password;
+    private boolean tieneCheckIn;
+    private int numeroHabitacion;
+
+    public Usuario(String email, String nombre, String password) {
+        //Sin inicializar para evitar errores de validacion 
+        this.email = "";
+        this.nombre = "";
+        this.password = "";
+        
+        //Validaciones en los sets
+        setEmail(email);
+        setNombre(nombre);
+        setPassword(password);
+        
+        this.tieneCheckIn = false;
+        this.numeroHabitacion = 0;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean setEmail(String email) {
+       if (email != null && email.contains("@") && email.contains(".")) {
+            this.email = email;
+            return true;
+        }
+        return false;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public boolean setNombre(String nombre) {
+       if (nombre != null && nombre.matches("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$")) {
+            this.nombre = nombre;
+            return true;
+        }
+        return false;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean setPassword(String password) {
+        if (password != null && password.length() >= 6) {
+            this.password = password;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isTieneCheckIn() {
+        return tieneCheckIn;
+    }
+
+    public void setTieneCheckIn(boolean tieneCheckIn) {
+        this.tieneCheckIn = tieneCheckIn;
+    }
+
+    public int getNumeroHabitacion() {
+        return numeroHabitacion;
+    }
+
+    public void setNumeroHabitacion(int numeroHabitacion) {
+        this.numeroHabitacion = numeroHabitacion;
+    }
+    
+    
     
 }
+
+
